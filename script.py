aa0b8cd208dac7803aeb6704e79ccb0ccb82971d
Yanina Aular <yanina.aular@vauxoo.com>
2013-05-21 09:27:44 -0430

    [IMP] mejoras de lectura de mensajes por consola

--- script.py
+++ script.py
@@ -21,50 +21,50 @@ from subprocess import Popen
 
 module = 'base_module_doc_rst'
 
-print 'Creando registro de revisiones donde se involucre el modulo %s...' % module
+print '\n\n*****************Creando registro de revisiones donde se involucre el modulo %s...' % module
 os.system('echo "revno" > revno.txt')
 os.system('bzr log addons/base_module_doc_rst | grep revno >> revno.txt')
 
-print 'Limpiando registro...'
+print '\n\n*****************Limpiando registro...'
 os.system('find revno.txt -type f -print0 | xargs -0 sed -i "s/revno: //g"')
 os.system('find revno.txt -type f -print0 | xargs -0 sed -i "s/ \[merge\]//g"')
 
-print 'Creando branch vacio a partir de addons...'
+print '\n\n*****************Creando branch vacio a partir de addons...'
 os.system('bzr branch addons/ %s -r 0' % (module,) )
 
-print 'Registrando commit inicial para poder hacer merge la primera vez...'
+print '\n\n*****************Registrando commit inicial para poder hacer merge la primera vez...'
 os.chdir(module)
 os.system('touch ci.txt')
 os.system('bzr add')
 os.system('bzr ci -m """main"""')
 
-print 'Leyendo registro de branches donde se involucra el modulo %s...' % module
+print '\n\n*****************Leyendo registro de branches donde se involucra el modulo %s...' % module
 archivo = csv.DictReader(open('../revno.txt'))
 
-print 'Preparando lista de revisiones...'
+print '\n\n*****************Preparando lista de revisiones...'
 invert = []
 for line in archivo:
     invert.append(line.get('revno'))
 invert.reverse()
 
-print 'Realizando primer merge...'
+print '\n\n*****************Realizando primer merge...'
 os.system('bzr merge ../addons/%s/ -r %s..%s' % (module,int(invert[0])-1,int(invert[0]),))
 
-print 'Limpiando commit de apoyo...'
+print '\n\n*****************Limpiando commit de apoyo...'
 os.system('bzr resolve %s' % module) 
 os.system('bzr uncommit --force')
 os.system('bzr rm ci.txt')
 os.system('bzr ci -m """[MERGE] from openobject-addons 6.1 revno %s"""' % int(invert[0]) )
 invert.remove( invert[0] ) 
 
-print 'Realizando merges restantes...'
+print '\n\n*****************Realizando merges restantes...'
 for rev in invert:
     print '\n******************************************\n'
     os.system('bzr merge ../addons/%s/ -r %s..%s' % (module,int(rev)-1,int(rev),))
     os.system('bzr resolve %s' % module) 
     os.system('bzr ci -m """[MERGE] from openobject-addons 6.1 revno %s"""' % rev )
 
-print 'Finalizando...'
+print '\n\n*****************Finalizando...'
 os.system('rm ../revno.txt')
 
-print 'Branch con modulo %s y sus revisiones fueron creadas correctamente!' % module 
+print '\n\n*****************Branch con modulo %s y sus revisiones fueron creadas correctamente!' % module 
